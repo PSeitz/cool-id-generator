1a041ae209095d557aa7b4e43a4b2768f0cc3f5b
mriise <mark.riise26@gmail.com>
2020-12-21 05:09:19 -0800

    refactor for less code repetition

--- src/prefix.rs
+++ src/prefix.rs
@@ -1,2 +1,12 @@
-pub(crate) const JOBS_PREFIX: [&'static str;7] = ["-robot","-laser","-turbo","-space","-steampunk","-jobless","-homeless"];
-pub(crate) const ANIMAL_PREFIX: [&'static str;8] = ["-robot","-mecha","-dino","-laser","-turbo","-rocket","-dressed","-space"];
+pub(crate) const JOBS_PREFIX: [&'static str; 7] = [
+    "-robot",
+    "-laser",
+    "-turbo",
+    "-space",
+    "-steampunk",
+    "-jobless",
+    "-homeless",
+];
+pub(crate) const ANIMAL_PREFIX: [&'static str; 8] = [
+    "-robot", "-mecha", "-dino", "-laser", "-turbo", "-rocket", "-dressed", "-space",
+];
