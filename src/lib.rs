5ac64a15539f101f1c499db08cc92cd19e30d2e9
mriise <mark.riise26@gmail.com>
2020-12-21 05:09:19 -0800

    refactor for less code repetition

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,14 @@ This is a cool-id-generator.
 It makes memorable ids.
 honest-turbo-tailor-gregory, romantic-robot-chicken-kenneth and happy-ultra-barista-shane would approve.
 
-```rust
+```
 #[macro_use]
-use cool_id_generator::{get_id, get_long_id};
+use cool_id_generator::{get_id, Size};
 
 fn main() {
-    let my_id = get_id();
+    let my_id = get_id(Size::Short);
     println!("{:?}", my_id);
-    let my_long_id = get_long_id();
+    let my_long_id = get_id(Size::Long);
     println!("{:?}", my_long_id);
 }
 ```
@@ -22,122 +22,95 @@ fn main() {
 extern crate alloc;
 use alloc::string::String;
 
-
-use crate::job::JOBS;
-use crate::animal::ANIMALS;
 use crate::adjective::ADJECTIVES;
+use crate::animal::ANIMALS;
+use crate::job::JOBS;
 use crate::names::NAMES;
-use crate::prefix::JOBS_PREFIX;
 use crate::prefix::ANIMAL_PREFIX;
+use crate::prefix::JOBS_PREFIX;
 
-use rand::thread_rng;
 use rand::seq::SliceRandom;
+use rand::thread_rng;
 
 mod adjective;
 mod animal;
-mod names;
 mod job;
+mod names;
 mod prefix;
 
-/// Creates ids in the format of {adjective}-{prefix}-{animal|job}-{name}
-/// e.g. "unpleasant-steampunk-poet-gerald"
-///
-/// Generates 1 billion combinations
+
 #[inline]
-pub fn get_id() -> String {
+pub fn get_id(size: Size) -> String {
     let mut rng = thread_rng();
 
-    let name = NAMES.choose(&mut rng).unwrap();
-    let adj1 = ADJECTIVES.choose(&mut rng).unwrap();
-    let animal = ANIMALS.choose(&mut rng).unwrap();
-    let job = JOBS.choose(&mut rng).unwrap();
+    let animal_or_job = {
+        if rand::random::<bool>() {
+            format!(
+                "{}-{}",
+                ANIMAL_PREFIX.choose(&mut rng).unwrap(),
+                ANIMALS.choose(&mut rng).unwrap()
+            )
+        } else {
+            format!(
+                "{}-{}",
+                JOBS_PREFIX.choose(&mut rng).unwrap(),
+                JOBS.choose(&mut rng).unwrap()
+            )
+        }
+    };
 
-    // let combinations_animals = NAMES.len() * ADJECTIVES.len() * ANIMALS.len();
-    // let combinations_jobs = NAMES.len() * ADJECTIVES.len() * JOBS.len();
-
-    let is_animal_or_job = rand::random();
-    if is_animal_or_job {
-        // It's an animal!
-        let prefix = get_animal_prefix(&mut rng);
-        // println!("Total Combinations {:?}", combinations_animals * ANIMAL_PREFIX.len());
-        format!("{}{}-{}-{}", adj1, prefix, animal, name)
-    }else{
-        // It's a job!
-        let prefix = get_job_prefix(&mut rng);
-        // println!("Total Combinations {:?}", combinations_jobs * JOBS_PREFIX.len());
-        format!("{}{}-{}-{}", adj1, prefix, job, name)
+    match size {
+        Size::Short => {
+            let adj1 = ADJECTIVES.choose(&mut rng).unwrap();
+            let name = NAMES.choose(&mut rng).unwrap();
+            format!("{}{}-{}", adj1, animal_or_job, name)
+        }
+        Size::Long => {
+            let name = NAMES.choose(&mut rng).unwrap();
+            let adj1 = ADJECTIVES.choose(&mut rng).unwrap();
+            let adj2 = ADJECTIVES.choose(&mut rng).unwrap();
+            format!("{}-the-{}-and-{}{}", name, adj1, adj2, animal_or_job)
+        }
+        Size::VeryLong => {
+            let name = NAMES.choose(&mut rng).unwrap();
+            let name2 = NAMES.choose(&mut rng).unwrap();
+            let adj1 = ADJECTIVES.choose(&mut rng).unwrap();
+            let adj2 = ADJECTIVES.choose(&mut rng).unwrap();
+            format!("{}-{}-the-{}-and-{}{}", name, name2, adj1, adj2, animal_or_job)
+        }
     }
-
 }
 
-/// Creates ids in the format of {name}-the-{adjective}-and-{adjective}-prefix-{animal|job}
-/// e.g. "unpleasant-steampunk-poet-gerald"
-///
-/// Generates 115 billion combinations
-#[inline]
-pub fn get_long_id() -> String {
-    let mut rng = thread_rng();
-
-    let name = NAMES.choose(&mut rng).unwrap();
-    let adj1 = ADJECTIVES.choose(&mut rng).unwrap();
-    let adj2 = ADJECTIVES.choose(&mut rng).unwrap();
-    let animal = ANIMALS.choose(&mut rng).unwrap();
-    let job = JOBS.choose(&mut rng).unwrap();
-
-    // let combinations_animals = NAMES.len() * ADJECTIVES.len() * ADJECTIVES.len() * ANIMALS.len();
-    // let combinations_jobs = NAMES.len() * ADJECTIVES.len() * ADJECTIVES.len() * JOBS.len();
-
-    let is_animal_or_job = rand::random();
-    if is_animal_or_job {
-        // It's an animal!
-        let prefix = get_animal_prefix(&mut rng);
-
-        // println!("Total Combinations {:?}", combinations_animals * ANIMAL_PREFIX.len());
-        format!("{}-the-{}-and-{}{}-{}", name, adj1, adj2, prefix, animal)
-    }else{
-        // It's a job!
-        let prefix = get_job_prefix(&mut rng);
-        // println!("Total Combinations {:?}", combinations_jobs * JOBS_PREFIX.len());
-        format!("{}-the-{}-and-{}{}-{}", name, adj1, adj2, prefix, job)
-    }
-
+/// Size which can be cast into `usize` to use as the size of the output byte array e.g.
+/// # Example
+/// ```
+/// use cool_id_generator::Size;
+/// 
+/// fn main() {
+///     let byte_array: [u8; Size::Short as usize] = [0u8; Size::Short as usize];
+/// }
+/// ```
+pub enum Size {
+    /// Creates ids in the format of {adjective}-{prefix}-{animal|job}-{name}
+    /// e.g. "unpleasant-steampunk-poet-gerald"
+    ///
+    /// Generates 1 billion combinations
+    Short = get_id_max_len() as isize,
+    /// Creates ids in the format of {name}-the-{adjective}-and-{adjective}-prefix-{animal|job}
+    /// e.g. "unpleasant-steampunk-poet-gerald"
+    ///
+    /// Generates 115 billion combinations
+    Long = get_long_id_max_len() as isize,
+    /// Creates ids in the format of {name1}-{name2}-the-{adjective}-and-{adjective}-prefix-{animal|job}
+    /// e.g. "unpleasant-steampunk-poet-gerald"
+    ///
+    /// Generates 10^15 combinations (or 2088136477473228)
+    VeryLong = get_very_long_id_max_len() as isize,
 }
 
-/// Creates ids in the format of {name1}-{name2}-the-{adjective}-and-{adjective}-prefix-{animal|job}
-/// e.g. "unpleasant-steampunk-poet-gerald"
-///
-/// Generates 10^15 combinations (or 2088136477473228)
-#[inline]
-pub fn get_very_long_id() -> String {
-    let mut rng = thread_rng();
-
-    let name = NAMES.choose(&mut rng).unwrap();
-    let name2 = NAMES.choose(&mut rng).unwrap();
-    let adj1 = ADJECTIVES.choose(&mut rng).unwrap();
-    let adj2 = ADJECTIVES.choose(&mut rng).unwrap();
-    let animal = ANIMALS.choose(&mut rng).unwrap();
-    let job = JOBS.choose(&mut rng).unwrap();
-
-    // let _combinations_animals = NAMES.len() * NAMES.len() * ADJECTIVES.len() * ADJECTIVES.len() * ANIMALS.len();
-    // let _combinations_jobs = NAMES.len() * NAMES.len() * ADJECTIVES.len() * ADJECTIVES.len() * JOBS.len();
-
-    let is_animal_or_job = rand::random();
-    if is_animal_or_job {
-        // It's an animal!
-        let prefix = get_animal_prefix(&mut rng);
-
-        // println!("Total Combinations {:?}", combinations_animals * ANIMAL_PREFIX.len());
-        format!("{}-{}-the-{}-and-{}{}-{}", name, name2, adj1, adj2, prefix, animal)
-    }else{
-        // It's a job!
-        let prefix = get_job_prefix(&mut rng);
-        // println!("Total Combinations {:?}", combinations_jobs * JOBS_PREFIX.len());
-        format!("{}-{}-the-{}-and-{}{}-{}", name, name2, adj1, adj2, prefix, job)
-    }
-
+const fn max(a: usize, b: usize) -> usize {
+    [a, b][(a < b) as usize]
 }
-
-const fn max(a: usize, b: usize) -> usize { [a, b][(a < b) as usize] }
 
 // returns maximum byte lengh of the given array
 const fn get_max_len(items: &[&str]) -> usize {
@@ -145,7 +118,9 @@ const fn get_max_len(items: &[&str]) -> usize {
     let mut largest = 0;
     while i < items.len() {
         let len = items[i].len();
-        if len > largest { largest = len };
+        if len > largest {
+            largest = len
+        };
         i += 1;
     }
     largest
@@ -154,46 +129,34 @@ const fn get_max_len(items: &[&str]) -> usize {
 #[inline]
 /// Returns the theoretical maximum byte length of the string returned by `get_very_long_id`
 pub const fn get_very_long_id_max_len() -> usize {
-    max(get_max_len(&ANIMAL_PREFIX), get_max_len(&JOBS_PREFIX)) +
-    get_max_len(&NAMES) + 
-    get_max_len(&NAMES) + 
-    get_max_len(&ADJECTIVES) + 
-    get_max_len(&ADJECTIVES) + 
-    get_max_len(&ANIMALS) + 
-    get_max_len(&JOBS)
+    max(get_max_len(&ANIMAL_PREFIX), get_max_len(&JOBS_PREFIX))
+        + get_max_len(&NAMES)
+        + get_max_len(&NAMES)
+        + get_max_len(&ADJECTIVES)
+        + get_max_len(&ADJECTIVES)
+        + get_max_len(&ANIMALS)
+        + get_max_len(&JOBS)
 }
 
 #[inline]
 /// Returns the theoretical maximum byte length of the string returned by `get_long_id`
 pub const fn get_long_id_max_len() -> usize {
-    max(get_max_len(&ANIMAL_PREFIX), get_max_len(&JOBS_PREFIX)) +
-    get_max_len(&NAMES) + 
-    get_max_len(&ADJECTIVES) + 
-    get_max_len(&ADJECTIVES) + 
-    get_max_len(&ANIMALS) + 
-    get_max_len(&JOBS) 
+    max(get_max_len(&ANIMAL_PREFIX), get_max_len(&JOBS_PREFIX))
+        + get_max_len(&NAMES)
+        + get_max_len(&ADJECTIVES)
+        + get_max_len(&ADJECTIVES)
+        + get_max_len(&ANIMALS)
+        + get_max_len(&JOBS)
 }
 
 #[inline]
 /// Returns the theoretical maximum byte length of the string returned by `get__id`
 pub const fn get_id_max_len() -> usize {
-    max(get_max_len(&ANIMAL_PREFIX), get_max_len(&JOBS_PREFIX)) +
-    get_max_len(&NAMES) + 
-    get_max_len(&ADJECTIVES) + 
-    get_max_len(&ANIMALS) + 
-    get_max_len(&JOBS)
-}
-
-#[inline]
-fn get_animal_prefix(rng: &mut rand::rngs::ThreadRng) -> &'static str {
-    let prefix = ANIMAL_PREFIX.choose(rng).unwrap();
-    prefix
-}
-
-#[inline]
-fn get_job_prefix(rng: &mut rand::rngs::ThreadRng) -> &'static str {
-    let prefix = JOBS_PREFIX.choose(rng).unwrap();
-    prefix
+    max(get_max_len(&ANIMAL_PREFIX), get_max_len(&JOBS_PREFIX))
+        + get_max_len(&NAMES)
+        + get_max_len(&ADJECTIVES)
+        + get_max_len(&ANIMALS)
+        + get_max_len(&JOBS)
 }
 
 #[cfg(test)]
@@ -240,4 +203,3 @@ mod tests {
         // println!("{}", get_very_long_id_max_len());
     }
 }
-
